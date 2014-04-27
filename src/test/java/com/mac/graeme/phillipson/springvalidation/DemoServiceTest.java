3565ca42e1329e77f43a103e1323f692b2e17152
Graeme Phillipson <graeme.phillipson@mac.com>
2014-04-27 14:46:04 +0100

    unused import

--- src/test/java/com/mac/graeme/phillipson/springvalidation/DemoServiceTest.java
+++ src/test/java/com/mac/graeme/phillipson/springvalidation/DemoServiceTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 @RunWith(SpringJUnit4ClassRunner.class)
